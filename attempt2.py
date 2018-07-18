4c6963ab8f3dd5d4f0aef64ecc3cdd9987f576b8
david.venator@yahoo.com <dvenator@lintilla.mdibl.net>
2018-07-18 10:56:55 -0400

    Working Script

--- attempt2.py
+++ attempt2.py
@@ -6,83 +6,108 @@ import scipy
 from scipy import stats
 import pickle
 
-INPuT = "steinmetz_paProb.txt"
+#inputs
 
-suffix = "paProb"
+table = "steinmetz_paProb.txt" 
+design = "Steinmetz_design.txt"
+suffix = "paProb" #names files how you want
+detail = ""  #if name in design file differs from column titles in table, write the addendem here
+genetics = ["cft2-1", "pcf11-2", "ipa1-1", "wt"] #the different groups
+Genetics = ["cft2_1", "pcf11_2", "ipa1_1", "wt"] #the different groups, replacing - with _ for file naming purposes
+control = "wt" #name the control, t-tests are run against it
 
-detail = ""
+dg = pd.read_csv(table, delimiter = "\t") #creates a pandas table from the text file for table
+df = pd.read_csv(design, delimiter = "\t") #creates a pandas table from the text tile for design
 
-control = "wt"
-
-dg = pd.read_csv(INPuT, delimiter = "\t") #creates a pandas table from the text file
-
-gene_array = dg["#gene"].tolist()
-
-inpUt = "Steinmetz_design.txt"
-
-#creates a pandas file from design file
-df = pd.read_csv(inpUt, delimiter = "\t")
-
-genetic_array = df["genetics"].tolist()
-
+gene_array = dg["#gene"].tolist()      	
+extra_array = dg["position"].tolist()    #print any added information, such as position on gene
+genetic_array = df["genetics"].tolist()  #the differentiator, such as genetics, as it appears as a column title in the design file
 sample_array = df["sample"].tolist()
 
-genetics = ["cft2-1", "pcf11-2", "ipa1-1", "wt"]
-
-#remove dashes, python doesnt like the - sign
-Genetics = ["cft2_1", "pcf11_2", "ipa1_1", "wt"]
-
 sample_names = []
+t_test_array = []
 
 #this loop finds which samples belong to each genetic type, and puts the list of names into a .data file containing a list of samples for the type. They are stored using pickle
 for s in range(0, len(genetics)):
   for i in range(0, len(genetic_array)):
     if genetic_array[i] == genetics[s]:
       sample_names.append(sample_array[i])
-  print(sample_names)
   with open(Genetics[s]+'.data', 'wb') as fp:
     pickle.dump(sample_names, fp)
   del sample_names[:]
 
-#this loop is supposed to combine use the sample titles found 
-#in the first loop to create one bug array for each genetic 
-#type - which worked, based on the print(bigarray) and np.mean(bigarray) working). 
-#The issue was that the big array will not save into a .data file, even though
-#im using essentially the same code from the previous loop.
+
+#reads the pickle file, and combines the arrays for each sample of a given genetic type, 
+
 
 for i in range(0, len(Genetics)) :
-  with open(Genetics[i]+'.data', 'rb') as fp:
-    Genetics[i] = []
-    Genetics[i].append(pickle.load(fp))
-    listitem = Genetics[i]
-    print(Genetics[i])
+  with open(Genetics[i]+'.data', 'rb') as fp4:
+    x  = []
+    x.append(pickle.load(fp4))
+    listitem = x
+    bigarray = []
     for s in range(0, len(listitem[0])):
       array = dg[listitem[0][s]+detail].tolist()
-      with open(genetics[i]+listitem[0][s]+'_single_array.data', 'wb') as fp:
-        pickle.dump(array, fp)
-      #print(genetics[i], listitem[0][s], array[1])
-      print(np.mean(array))
-      bigarray = np.hstack((array))
-      #del array[:]
-    print(genetics[i], "mean - ", np.mean(bigarray))
-    with open(genetics[i]+'_combined.data', 'wb') as fp:
-      pickle.dump(bigarray, fp)  				
+      with open(genetics[i]+listitem[0][s]+'_single_array.data', 'wb') as fp1:
+        pickle.dump(array, fp1)
+      with open(suffix+'_means.txt', 'a+') as f:
+        print(listitem[0][s], "	", np.mean(array), file=f)
+      bigarray  = np.hstack((bigarray,array))
+      del array[:]
+    with open(suffix+'_means.txt', 'a+') as f:
+      print("	", file=f)
+    with open(suffix+'_means.txt', 'a+') as f:
+      print(genetics[i], "mean - ", np.mean(bigarray), file=f)
+    with open(suffix+'_means.txt', 'a+') as f:
+      print("	", file=f)
+    with open(genetics[i]+'_combined.data', 'wb') as fp2:
+      pickle.dump(bigarray, fp2)  				
 
+fp.close()
+fp1.close()
+fp2.close()
 
-#meant to assign a variable to the control array, once that works
 with open(control+'_combined.data', 'rb') as fp:
-  control_array = pickle.load(fp)
+  Control_array = pickle.load(fp)
 
-#this is the t-test function, meant to compare each of the mutations to the wildtype. doesnt work yet because of the issues storing the bigarray in the previous loop.
+
 for a in range(0, len(Genetics)) :
    if Genetics[a] == control:
-     stop
+     print("nope")
    else:
-     with open(genetics[a]+'_combined.data', 'rb') as fp:
-       Genetics[a] = []
-       Genetics[a].append(pickle.load(fp))
-       answer = scipy.stats.ttest_ind(control_array, Genetics[a][0], equal_var = False)
+     with open(genetics[a]+'_combined.data', 'rb') as fp3:
+       f = []
+       f.append(pickle.load(fp3))
+       answer = scipy.stats.ttest_ind(Control_array, f[0], equal_var = False)
        with open('T-Test_'+suffix+detail+'.txt', 'a+') as f:
            print(genetics[a], "vs WT unequal variance t-test result        ", answer, file=f)
 
+for n in range(0, len(gene_array)) : 
+  t_test_array = []
+  control_array = []
+  for i in range(0, len(Genetics)) :
+    with open(Genetics[i]+'.data', 'rb') as fp5:   #this line is broken
+      h = []
+      h.append(pickle.load(fp5))
+      listitem = h
+      if genetics[i] == control:
+        control_genetic_array = []
+        for s in range(0, len(listitem[0])):
+          with open(genetics[i]+listitem[0][s]+'_single_array.data', 'rb') as fp1:
+            Control = pickle.load(fp1)
+            control_genetic_array.append(Control[n])
+        control_array.append(control_genetic_array)
+      else:
+        gene_genetic_array = []
+        for s in range(0, len(listitem[0])):
+          with open(genetics[i]+listitem[0][s]+'_single_array.data', 'rb') as fp1:
+            array = pickle.load(fp1)
+            gene_genetic_array.append(array[n])
+        t_test_array.append(gene_genetic_array)
+  for a in range(0, len(t_test_array)) :
+     genetics.remove(control)
+     answer = scipy.stats.ttest_ind(control_array[0], t_test_array[a], equal_var = False)
+     with open('Gene_based_T-Test_'+suffix+detail+'.txt', 'a+') as f:
+        print(gene_array[n], extra_array[n], genetics[a], "vs wt unequal variance t-test result        ", answer, file=f)  
+     genetics.append(control)
 
